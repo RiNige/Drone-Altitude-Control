b6fc420cf54d4581a5ae3086981c778c2f357ff3
Tian Wang <912766017@qq.com>
2020-02-11 12:22:23 +0000

    first commit

---
+++ LidDrivenCavitySolver.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+#include "LidDrivenCavity.h"
+
+int main(int argc, char **argv)
+{
+    // Create a new instance of the LidDrivenCavity class
+    LidDrivenCavity* solver = new LidDrivenCavity();
+
+    // Configure the solver here...
+    // 
+    int check = 0;
+    try{
+    // read input from user and check validity 
+    solver->Initialise();
+    check = solver->validityCheck();
+    if (check == 0){
+        throw std::logic_error("dt must smaller than re*dx*dy/4");
+    }
+    else{
+        cout << "Input Parameters pass the validation check" << endl;
+    }
+    // Run the solver
+    solver->Integrate();
+    }
+    catch(const std::logic_error & e){
+        cout << "Error: " << e.what() << endl;
+    }
+	return 0;
+}
